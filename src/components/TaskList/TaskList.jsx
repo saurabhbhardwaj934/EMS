56d9f9e6e561e22ea927c77dc7b1acf703b55d88
saurabhbhardwaj934 <saurabhbhardwaj067@gmail.com>
2026-02-16 15:11:16 +0530

    add horizontal scroll in the tasklist

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className=' h-60 bg-amber-500 rounded-xl p-6 text-white'>
+    <div id='tasklist' className='flex items-center justify-center overflow-x-auto h-60 py-5 gap-4 rounded-xl p-6 text-white mt-10 flex-nowrap'>
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-500 rounded-xl'>
+            <div className='flex justify-between'>
+              <h3 className='bg-red-600 '>High</h3>
+              <h4> 20 feb 2026 </h4>
+            </div>
+            
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-500 rounded-xl'>
+            <div className='flex justify-between'>
+              <h3 className='bg-red-600 '>High</h3>
+              <h4> 20 feb 2026 </h4>
+            </div>
+            
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-500 rounded-xl'>
+            <div className='flex justify-between'>
+              <h3 className='bg-red-600 '>High</h3>
+              <h4> 20 feb 2026 </h4>
+            </div>
+            
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-500 rounded-xl'>
+            <div className='flex justify-between'>
+              <h3 className='bg-red-600 '>High</h3>
+              <h4> 20 feb 2026 </h4>
+            </div>
+            
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-500 rounded-xl'>
+            <div className='flex justify-between'>
+              <h3 className='bg-red-600 '>High</h3>
+              <h4> 20 feb 2026 </h4>
+            </div>
+            
+      </div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-500 rounded-xl'>
+            <div className='flex justify-between'>
+              <h3 className='bg-red-600 '>High</h3>
+              <h4> 20 feb 2026 </h4>
+            </div>
+            
+      </div>
+      
+            
+      
       
     </div>
   )
