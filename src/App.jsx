8d05284b127e76692fd361d7672e4b76e5d736b3
saurabhbhardwaj934 <saurabhbhardwaj067@gmail.com>
2026-02-18 14:37:14 +0530

    initialized localStorage in App using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import Login from './components/Auth/Login' 
+import React, { useEffect } from 'react'
+import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
 
 const App = () => {
+  useEffect(() => {
+  setLocalStorage();
+  getLocalStorage()
+  
+}, []);
+
+
   return (
     <>
-    {/* <Login/> */}
-    {/* <EmployeeDashboard/>   */}
-    {/* <AdminDashboard/> */}
-    
+      <Login />
 
-    
-    
-    
-    
-    
+      {/* <EmployeeDashboard/>   */}
+      {/* <AdminDashboard/> */}
+
+
+
+
+
+
+
     </>
   )
 }
