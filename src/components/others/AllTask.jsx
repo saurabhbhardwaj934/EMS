ac1dfd7450ded0d272001827f808c739042d8005
saurabhbhardwaj934 <saurabhbhardwaj067@gmail.com>
2026-02-18 14:22:56 +0530

    Added Auth provider with children wrapper

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const AllTask = () => {
+    return (
+        <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-y-auto'>
+            <div className='bg-red-400 py-2 px-4 mb-4 flex justify-between rounded text-white'>
+
+                <h2>Saurabh</h2>
+                <h3> Make a UI Dsign</h3>
+                <h5>Status</h5>
+
+
+            </div>
+             <div className='bg-yellow-400 py-2 px-4 mb-4 flex justify-between rounded text-white'>
+
+                <h2>Saurabh</h2>
+                <h3> Make a UI Dsign</h3>
+                <h5>Status</h5>
+
+
+            </div>
+             <div className='bg-green-400 py-2 px-4 mb-4 flex justify-between rounded text-white'>
+
+                <h2>Saurabh</h2>
+                <h3> Make a UI Dsign</h3>
+                <h5>Status</h5>
+
+
+            </div>
+             <div className='bg-purple-400 py-2 px-4 mb-4 mb-4 flex justify-between rounded text-white'>
+
+                <h2>Saurabh</h2>
+                <h3> Make a UI Dsign</h3>
+                <h5>Status</h5>
+
+
+            </div>
+             <div className='bg-sky-400 py-2 px-4 flex mb-4 justify-between rounded text-white'>
+
+                <h2>Saurabh</h2>
+                <h3> Make a UI Dsign</h3>
+                <h5>Status</h5>
+
+
+            </div>
+             <div className='bg-emerald-400 py-2 px-4 mb-4 flex justify-between rounded text-white'>
+
+                <h2>Saurabh</h2>
+                <h3> Make a UI Dsign</h3>
+                <h5>Status</h5>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default AllTask
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthContext = ({ children }) => {
+  return (
+    <div>
+        {children}
+        
+
+    </div>
+  )
+}
+
+export default AuthContext
